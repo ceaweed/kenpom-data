c891baacd9b9e2899002f88539d201035d883c26
ceaweed <cwellen.w@gmail.com>
2023-03-30 20:53:52 -0500

    Working

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
+import csv
+
 from bs4 import BeautifulSoup
 import requests
+
+filename = 'outputWeb.csv'
+
 url = 'https://kenpom.com/index.php'
 header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36 Edg/111.0.1661.44'}
-html_text = requests.get(url, headers=header)
+html_text = requests.get(url, headers=header).text
 
-print(html_text)
+soup = BeautifulSoup(html_text, 'lxml')
+teams = soup.findAll('td', class_ = 'next_left')
+
+with open(filename, 'w', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    for team in teams:
+        csvwriter.writerow([team.text])
 
